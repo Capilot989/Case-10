92f12b44e79461d10b7db1e15bd056b29d50c09e
slavagarchenko <cvgarchenko@gmail.com>
2025-10-28 18:00:15 +0700

    Update and rename role_3.py to analyze_finance.py

--- analyze_finance.py
+++ analyze_finance.py
@@ -1,13 +1,12 @@
-# автоматически создает занчения для несуществующих ключей, может быть использован не только для списков
 from collections import defaultdict
 from datetime import datetime
 
 
 def calculate_basic_stats(transactions: list) -> dict:
-    total_income = sum(transaction['amount']
-                       for transaction in transactions if transaction['amount'] > 0)
-    total_expense = sum(transaction['amount']
-                        for transaction in transactions if transaction['amount'] < 0)
+    total_income = sum(transaction['Сумма']
+                       for transaction in transactions if transaction['Сумма'] > 0)
+    total_expense = sum(transaction['Сумма']
+                        for transaction in transactions if transaction['Сумма'] < 0)
     balance = total_income + total_expense
     transaction_count = len(transactions)
 
@@ -22,12 +21,14 @@ def calculate_basic_stats(transactions: list) -> dict:
 def calculate_by_category(transactions: list) -> dict:
     category_stats = defaultdict(
         lambda: {'sum': 0, 'count': 0, 'expense_percent': 0, 'income_percent': 0})
-    total_expense = sum(t['amount'] for t in transactions if t['amount'] < 0)
-    total_income = sum(t['amount'] for t in transactions if t['amount'] > 0)
+    total_expense = sum(transaction['Сумма'] 
+                        for transaction in transactions if transaction['Сумма'] < 0)
+    total_income = sum(transaction['Сумма'] 
+                       for transaction in transactions if transaction['Сумма'] > 0)
 
     for transaction in transactions:
-        category = transaction['category']
-        category_stats[category]['sum'] += transaction['amount']
+        category = transaction['Сумма']
+        category_stats[category]['sum'] += transaction['Сумма']
         category_stats[category]['count'] += 1
 
     for category, data in category_stats.items():
@@ -48,11 +49,11 @@ def analyze_by_time(transactions: list) -> dict:
     monthly_stats = defaultdict(lambda: {'income': 0, 'expenses': 0})
 
     for transaction in transactions:
-        date_obj = datetime.strptime(transaction['date'], '%Y-%m-%d')
+        date_obj = datetime.strptime(transaction['Дата'], '%d.%m.%Y')
         month_str = date_obj.strftime('%Y-%m')
-        if transaction['amount'] > 0:
-            monthly_stats[month_str]['income'] += transaction['amount']
+        if float(transaction['Сумма']) > 0:
+            monthly_stats[month_str]['income'] += float(transaction['Сумма'])
         else:
-            monthly_stats[month_str]['expenses'] += abs(transaction['amount'])
+            monthly_stats[month_str]['expenses'] += abs(float(transaction['Сумма']))
 
     return dict(monthly_stats)
